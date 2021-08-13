7390aba04331d5d6033c59381199eaaf67b8e34b
Vinicio Rojas <rojas.gt@gmail.com>
2021-08-13 13:11:41 -0600

    test throw

--- node-jest/index.js
+++ node-jest/index.js
@@ -1,1 +1,19 @@
-module.exports = char => 'aeiou'.includes(char);
+exports.isVowel= char => {
+    if(typeof char !== 'string') {
+        throw TypeError('Arg must be a string');
+    }
+    if(char.length !== 1) {
+        throw Error('Arg length must be 1');
+    }
+
+    return 'aeiou'.includes(char)
+};
+
+exports.countVowels =  str => [...str].reduce(
+    (memo, char) => (
+        memo.hasOwnProperty(char) 
+            ? { ...memo, [char]: memo[char] + 1 }
+            : memo
+    ),
+    { a: 0, e: 0, i: 0, o: 0, u: 0 },
+);
--- node-jest/isVowel.spec.js
+++ node-jest/isVowel.spec.js
@@ -1,13 +1,26 @@
-const isVowel = require('.');
+const { expect } = require('@jest/globals');
+const { isVowel, countVowels } = require('.');
 
 describe('isVowel', () => {
-    it('should returns true for input "a"', () => {
-        expect(isVowel('a')).toBe(true);
+    it('should throws when hasn\'t parameter', () => {
+        expect(() => isVowel()).toThrow(TypeError);
     });
-    
+
+    it('should throws when string length is > 1', () => {
+        expect(() => isVowel('aaa')).toThrow(Error);
+    });
+
+    it('should returns true for input "a"', () => {
+        expect(isVowel('a')).toBeTruthy();
+    });  
     
     it('should returns false for input "c"', () => {
         expect(isVowel('c')).toBe(false);
     });
 });
 
+describe('countVowels', () => {
+    it('should...', () => {
+        expect(countVowels('abc')).toEqual({ a: 1, e: 0, i: 0, o: 0, u: 0 });
+    });
+});
